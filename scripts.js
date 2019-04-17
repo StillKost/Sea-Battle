4cf79dda77580e408eb561a3963da1d700c5bd97
Кавказская Чичевамба <hetagurovkost777@mail.ru>
2019-04-17 11:36:10 +0300

    commit

--- scripts.js
+++ scripts.js
@@ -1,63 +1,40 @@
-//по анализу данных!!!
+﻿//по анализу данных!!!
 //x - то что влияет y - незавсимая
 $(document).ready(function (argument) {
 	for (var i = 0; i < 10; i++){
 		$(".field").append('<div class="field-row"><div class="square"></div><div class="square"></div><div class="square"></div><div class="square"></div><div class="square"></div><div class="square"></div><div class="square"></div><div class="square"></div><div class="square"></div><div class="square"></div></div>');
 	}
+
+
 	//5 - 1p 4 - 2p 3 - 3p 2 - 4p 1 - 5p
 	
 	//раскидываю однопалубные юзеру
 
-	//основное поле игрока
+	//поле игрока по квадратам
 	var userSquares = $("#user-user").find(".square");
-	//второе поле игрока
+
 	var cpuUserSquares = $("#cpu-user").find(".square");
-	//основное поле CPU
 	var cpuSquares = $("#cpu-cpu").find(".square");
-	//второе поле CPU
 	var userCpuSquares = $("#user-cpu").find(".square");
 
-
-	//основное поле игрока в виде матрицы
-	var userMatrix = TenToTen(userSquares, 10);
-	//основное поле CPU в виде матрицы
-	var cpuMatrix = TenToTen(cpuSquares, 10);
-
-
-
-	for(var i = 0; i < userMatrix.length; i++){
-		for(var j = 0; j < userMatrix.length; j++){
-			if(i % 2 == 0){
-				var rand = RandomInt(0,11);
-				$(userMatrix[i][rand]).addClass('ship');
-				break;
-			}
+	//матрицы 10х10
+	var userField = TenToTen(userSquares, 10);
+	var userCpuField = TenToTen(userCpuSquares, 10);
+	//раскидываю однопаллубные
+	for(var i = 0; i < userField.length; i++){
+		if(i % 2 == 0){
+			var random = RandomInt(0,10);
+			$(userField[i][random]).addClass('ship-single');
+			$(userCpuField[i][random]).addClass('ship-hidden');
 		}
 	}
-
-
-
-
-
-
-
-
-	//раскидываю однопалубные cpu
-/*	var cpuSquares = $("#cpu-cpu").find(".square");
-	var userCpuSquares = $("#user-cpu").find(".square");
-	for (var i = 0; i < cpuSquares.length; i++) {
-		if(i % 20 == 0){
-			var rand = Math.floor(Math.random() * userSquares.length);
-			$(cpuSquares[rand]).addClass('ship');
-			$(userCpuSquares[rand]).addClass('ship-hidden');
+	var anySquares = $("#user-user").find('.square:not(.ship-single)');
+	//раскидываю двухпаллубные
+	for(var i = 0; i < anySquares.length; i++){
+		for(var j = 0; j < anySquares.length; j++){
+			var flag = RandomInt(0,100) % 2 ? true : false;
 		}
-	}*/
-
-
-
-
-
-
+	}
 
 
 
